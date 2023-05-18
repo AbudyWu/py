770de8c3910addc656fa62ae981a3f9afb8436c3
AbudyWu <B11007143@gapps.ntust.edu.tw>
2023-05-18 11:27:17 +0800

    dev1.1

--- 2a2b.py
+++ 2a2b.py
@@ -11,11 +11,11 @@ def generate():
     result=num[:4]
     return result
 ans = generate()
-#print('ans=',ans)
+print('ans=',ans)
 while 1==1:
     guess = [int(s) for s in input('key 4 number:')]
     while len(guess) != 4:
-        print("Please enter four digits")
+        print('\033[31;46m error pls input 4 digit number \033[0m')
         guess = [int(s) for s in input('key 4 number:')]
     for i in range(4):
         for j in range(4):
@@ -23,7 +23,7 @@ while 1==1:
             if ans[i] == guess[j] and i!=j:b+=1
     print(a,'A',b,'B')
     if a==4:
-        print('yes ans is',*ans)
+        print('\033[33;45m YES ANS IS :\033[0m',*ans)
         break
     a=0
     b=0
